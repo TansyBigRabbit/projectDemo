86ebd4a63e768e0f408c25a70956c005c8f982be
Tansy <chenhongying@dhcc.com.cn>
2019-06-09 21:52:26 +0800

    上传代码

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,104 +1,118 @@
 <template>
-<el-container style="height: 100vh; border: 1px solid #eee">
-  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-    <el-menu style="height: 100vh;" :default-openeds="['1', '3']">
-      <el-submenu index="1">
-        <template slot="title"><i class="el-icon-message"></i>导航一</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-submenu index="2">
-        <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="2-1">选项1</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="2-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title"><i class="el-icon-setting"></i>导航三</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="3-1">选项1</el-menu-item>
-          <el-menu-item index="3-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="3-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="3-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-    </el-menu>
-  </el-aside>
-  
-  <el-container>
-    <el-header style="text-align: right; font-size: 12px">
+<el-container>
+  <el-header style="text-align: right; font-size: 12px"> 
       <el-dropdown>
         <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>查看</el-dropdown-item>
-          <el-dropdown-item>新增</el-dropdown-item>
-          <el-dropdown-item>删除</el-dropdown-item>
-        </el-dropdown-menu>
+       <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>我的消息</el-dropdown-item>
+            <el-dropdown-item>设置</el-dropdown-item>
+            <el-dropdown-item divided >退出登录</el-dropdown-item>
+          </el-dropdown-menu>
       </el-dropdown>
       <span>王小虎</span>
     </el-header>
-    
+  <el-container>
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+    <el-menu style="height: 100vh;" :default-openeds="['1', '3']">
+      <el-submenu index="1">
+        <template slot="title"><i class="el-icon-message"></i>信访中心系统</template>
+        <el-menu-item-group> 
+          <el-menu-item index="1-1">
+            <router-link tag="span" :to="'/home/lettersVisit'">开启信访</router-link>
+          </el-menu-item>
+          <el-menu-item index="1-2">信访接访</el-menu-item>
+          <el-menu-item index="1-3">信访处理</el-menu-item>
+        </el-menu-item-group>  
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title"><i class="el-icon-menu"></i>党建中心系统</template>
+        <el-menu-item-group> 
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+        </el-menu-item-group>  
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title"><i class="el-icon-setting"></i>综治中心系统</template>
+        <el-menu-item-group> 
+          <el-menu-item index="3-1">选项1</el-menu-item>
+          <el-menu-item index="3-2">选项2</el-menu-item>
+          <el-menu-item index="3-3">选项3</el-menu-item>
+        </el-menu-item-group>  
+      </el-submenu>
+      <el-submenu index="4">
+        <template slot="title"><i class="el-icon-setting"></i>用户管理</template>
+        <el-menu-item-group> 
+          <el-menu-item index="4-1">选项1</el-menu-item>
+          <el-menu-item index="4-2">选项2</el-menu-item>
+        </el-menu-item-group>  
+      </el-submenu>
+      <el-submenu index="5">
+        <template slot="title"><i class="el-icon-setting"></i>系统管理</template>
+        <el-menu-item-group> 
+          <el-menu-item index="5-1">选项1</el-menu-item>
+          <el-menu-item index="5-2">选项2</el-menu-item>
+        </el-menu-item-group>  
+      </el-submenu>
+    </el-menu>
+  </el-aside>
     <el-main>
-      <el-table :data="tableData">
+    <router-view></router-view>
+      <!-- <el-table :data="tableData">
         <el-table-column prop="date" label="日期" width="140">
         </el-table-column>
         <el-table-column prop="name" label="姓名" width="120">
         </el-table-column>
         <el-table-column prop="address" label="地址">
         </el-table-column>
-      </el-table>
-    </el-main>
+      </el-table> -->
+    </el-main> 
   </el-container>
-</el-container>
+</el-container>  
 </template>
 <style>
   .el-header {
-    background-color: #B3C0D1;
+    background-color: #1890F5;
     color: #333;
     line-height: 60px;
+    height: 55px;
   }
   
   .el-aside {
-    color: #333;
+    color: #ffffff;
+    background-color: #001529
   }
+  .el-menu{
+    background-color: #001529
+  }
+  .el-submenu__title,.el-menu-item{
+    color:#ffffff;
+  }
+  .el-menu-item:focus, .el-menu-item:hover ,
+  .el-submenu__title:focus,.el-submenu__title:hover{
+    outline: 0;
+    background-color: #1890F5;
+    color:#ffffff;
+  }
+  .el-menu-item-group__title {
+    padding: 0;
+    padding-left: 0px;
+    line-height: normal;
+    font-size: 12px; 
+} 
 </style>
 
 <script>
   export default {
     data() {
-      const item = {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      };
       return {
-        tableData: Array(20).fill(item)
+          
       }
+    },
+    methods: { 
+    },
+    mounted() {
+     
     }
-  };
+  }
+
 </script>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Login from './components/login'
 import Home from './components/Home'
+import LettersVisit from './components/lettersVisit/lettersVisit'
 export const routes = [{
 	path: '/',
 	name: 'login',
@@ -7,5 +8,14 @@ export const routes = [{
 }, {
 	path: '/home',
 	name: 'home',
-	component: Home
+	component: Home,
+	children: [{
+		path: '/home/lettersVisit',
+		name: 'lettersVisit',
+		component: LettersVisit,
+		/*meta: {
+		  bcrumd: ['首页'],
+		  activePath: "/menu/home"
+		}*/
+	}, ]
 }];
